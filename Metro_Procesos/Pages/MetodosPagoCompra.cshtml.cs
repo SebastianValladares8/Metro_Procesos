d1b64024526593572f570082818361d0a3121287
SebastianValladares8 <fsvalladaresp@@utn.edu.ec>
2026-01-19 23:25:59 -0500

    Actualizacion 0.0.0.2

--- Metro_Procesos/Pages/MetodosPagoCompra.cshtml.cs
+++ Metro_Procesos/Pages/MetodosPagoCompra.cshtml.cs
@@ -3,7 +3,7 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace Metro_Procesos.Pages
 {
-    public class CompraSaldoModel : PageModel
+    public class MetodosPagoCompraModel : PageModel
     {
         public void OnGet()
         {
